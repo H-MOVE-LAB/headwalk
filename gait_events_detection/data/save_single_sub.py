eb0b09983f135b7f7201b5a6bd7972818b89f6e6
paolotasca <paolo.tasca@polito.it>
2026-01-30 09:17:57 +0000

    added gyro example data

--- gait_events_detection/data/save_single_sub.py
+++ gait_events_detection/data/save_single_sub.py
@@ -7,26 +7,26 @@ import numpy as np
 
 from load_icicle_trial_with_gait_events import load_icicle_trial_with_gait_events
 
-PROJECT_ROOT = Path(__file__).resolve().parents[3]
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 h5_file = PROJECT_ROOT / "data" / "ICICLE-Gait_F2_SC" / "Controls" / \
           "20140616-104931-INGC116F2_SC.h5"
-h5_file = PROJECT_ROOT / "data" / "ICICLE-Gait_F2_SC" / "PDs" / \
-          "20140430-142918-INGP065F2_SC.h5"
+# h5_file = PROJECT_ROOT / "data" / "ICICLE-Gait_F2_SC" / "PDs" / \
+#           "20140430-142918-INGP065F2_SC.h5"
 gaitrite_excel = PROJECT_ROOT / "metadata" / "Paolo_step_data.xlsx"
 
-# Load WT accelerometer (z-axis only), all Gait_Id
+# Load Head acc or gyro data, all Gait_Id
 time_sec, data, events = load_icicle_trial_with_gait_events(
     h5_file=h5_file,
     gaitrite_excel=gaitrite_excel,
     sensor="HD",
-    signal="acc",
+    signal="gyr",
     gait_id=None,
     plot=False
 )
 
 # Save for future ML / DL usage
-out_file = "INGP065_F2_SC_HD_acc.npz"
+out_file = "INGC116_F2_SC_HD_gyr.npz"
 print(f"Saving...")
 
 np.savez(
